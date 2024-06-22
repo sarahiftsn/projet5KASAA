b3ac87a033b036d8656fe6d7fda79b3ee4485dd7
iftissene <iftissenesarah4@gmail.com>
2024-06-22 12:57:01 +0200

    error404

--- projetavecvite/src/assets/Pages/Home.jsx
+++ projetavecvite/src/assets/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../../components/Navbar";
 import Banner from "../../layout/Banner";
 import Cards from "../../components/Cards";
 import Footer from "../../layout/Footer";
+import "./Home.scss";
 
 
 
--- projetavecvite/src/assets/Pages/Page404.jsx
+++ projetavecvite/src/assets/Pages/Page404.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Logements from './Logements';
+import { Link } from 'react-router-dom';
+import "./Page404.scss";
 
 
 
@@ -9,8 +11,10 @@ function Error () {
       <>
       <div className='img'>
       <img src="/404.png" alt="error" />
+      </div>
+      <div className='texts'>
       <p>Oups! La page que vous demandez n'existe pas</p>
-      <p>Retourner sur la page d’accueil</p>
+      
       </div>
       </>
     );
