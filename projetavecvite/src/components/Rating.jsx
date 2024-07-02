1ec680cd68687114cb757015aa6aa985e38e98a1
iftissene <iftissenesarah4@gmail.com>
2024-07-02 17:14:50 +0200

    amélioration

--- projetavecvite/src/components/Rating.jsx
+++ projetavecvite/src/components/Rating.jsx
@@ -6,7 +6,7 @@ import { faStar } from '@fortawesome/free-solid-svg-icons';
 function Rating({ rating }) {
     const roundedRating = Math.round(rating); // Arrondir la note au nombre entier le plus proche
     const stars = [...Array(5)].map((_, index) => { // Créer un tableau de 5 éléments pour représenter les étoiles
-        const starClass = index < roundedRating ? 'star-filled' : 'star-empty'; // Déterminer la classe CSS en fonction de la note
+        const starClass = index < roundedRating ? 'star star-filled' : 'star star-empty'; // Déterminer la classe CSS en fonction de la note
         return <FontAwesomeIcon key={index} icon={faStar} className={starClass} />; // Retourner l'icône d'étoile avec la classe appropriée
     });
 
