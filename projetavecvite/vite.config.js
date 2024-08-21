71a71748421c56d1abe6ed989fa01a9be70623f9
iftissene <iftissenesarah4@gmail.com>
2024-08-22 01:04:31 +0200

    Configurer le déploiement GitHub Pages

--- projetavecvite/vite.config.js
+++ projetavecvite/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/projet5KASAA/', // Remplacez par le nom de votre dépôt GitHub
   plugins: [react()],
 })
